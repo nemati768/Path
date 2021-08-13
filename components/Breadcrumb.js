4e9df5ca8b5134588719938e0ee1c82efad138d6
Saeed Nemati <sn.1361@gmail.com>
2021-08-13 20:04:02 +0430

    committed by script

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -11,12 +11,14 @@ const Breadcrumb = ({ urlSegments }) => {
             text: urlSegments[i].split('-').map(i => i.charAt(0).toUpperCase() + i.slice(1)).join(' ')
         })
     }
-    return <div className="breadcrumb">
-            <a href="/">Home</a><span>&gt;</span>
-        {
-            items.map(i => <span key={i.url}>
-            <a href={i.url + '/index'}>{i.text}</a><span>&gt;</span></span>)
-        }
+    return <div className="breadcrumb" className='h-20 flex items-center pl-5'>
+        <div>
+            <a href="/">Home</a><span className="px-2">&gt;</span>
+            {
+                items.map(i => <span key={i.url}>
+                    <a href={i.url + '/index'}>{i.text}</a><span className="px-2">&gt;</span></span>)
+            }
+        </div>
     </div>
 }
 
